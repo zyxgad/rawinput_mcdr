28656f42567ebe9c30bcd50d006409ac9662a0af
zyxgad <zyx1z2@163.com>
2021-08-13 01:28:59 -0600

    v1.0.2

--- RawInput.py
+++ RawInput.py
@@ -14,13 +14,10 @@ else:
 	import termios
 
 import mcdreforged.api.all as MCDR
-# from mcdreforged.executor.console_handler import ConsoleHandler
-# from mcdreforged.info import Info
-# from mcdreforged.utils.logger import DebugOption, SyncStreamHandler
 
 PLUGIN_METADATA = {
   'id': 'rawinput',
-  'version': '1.0.1',
+  'version': '1.0.2',
   'name': 'RawInput',
   'description': 'Minecraft better console!',
   'author': 'zyxgad',
@@ -71,12 +68,14 @@ MOVE_LEFT_CHAR = '\x1b\x5b\x44'
 
 class RawReader(io.RawIOBase):
 	def __init__(self, reader, writer, prefix='', *, mixin=False):
-		self._isend = False
-		self._helper_thr = None
+		self.__isinterrupt = False
+		self.__isclose = False
+		self.__helper_thr = None
 		self._prefix = prefix
 		self._mixin_map = {}
 		self._mixin = mixin
-		self._old_settings = None
+		self.__old_settings = None
+		self.__pid = os.getpid()
 		self._buffer_lock = threading.RLock()
 		self._empty_lock = threading.Condition(self._buffer_lock)
 		self._write_lock = threading.RLock()
@@ -86,27 +85,25 @@ class RawReader(io.RawIOBase):
 		self.__current_his = 0
 		self.__line_buf = []
 		self.__line_index = 0
-		assert reader.readable()
+		assert reader.readable() and reader.isatty(), "输入流必须是(伪)终端设备"
 		self._reader = reader
 		self._reader_fd = self._reader.fileno()
 		assert writer.writable()
 		self._writer = writer
 		if not IS_WIN32:
-			self._old_settings = termios.tcgetattr(self._reader_fd)
+			self.__old_settings = termios.tcgetattr(self._reader_fd)
 			try:
 				mode = termios.tcgetattr(self._reader_fd)
 				mode[tty.IFLAG] &= ~(termios.BRKINT | termios.INPCK | termios.IXON)
 				mode[tty.IFLAG] |= termios.ICRNL
-				mode[tty.OFLAG] |= termios.BSDLY
 				mode[tty.CFLAG] &= ~(termios.CSIZE | termios.PARENB)
 				mode[tty.CFLAG] |= termios.CS8
-				mode[tty.LFLAG] &= ~(termios.ICANON | termios.ECHO)
-				mode[tty.LFLAG] |= termios.ISIG
+				mode[tty.LFLAG] &= ~(termios.ICANON | termios.ECHO | termios.ISIG)
 				mode[tty.CC][termios.VMIN] = 1
 				mode[tty.CC][termios.VTIME] = 0
 				termios.tcsetattr(self._reader_fd, termios.TCSADRAIN, mode)
 			except:
-				self.end()
+				self.close()
 				raise
 		if self._mixin:
 			self.__mix(self._reader, 'read')
@@ -114,10 +111,10 @@ class RawReader(io.RawIOBase):
 			self.__mix(self._reader, 'readlines')
 			self.__mix(self._writer, 'write')
 			self.__mix(self._writer, 'writelines')
-		self._helper_thr = threading.Thread(target=self.__helper, name='raw-reader(r:{},w:{})'.
+		self.__helper_thr = threading.Thread(target=self.__helper, name='raw-reader(r:{},w:{})'.
 			format(self._reader.fileno(), self._writer.fileno()))
-		self._helper_thr.setDaemon(True)
-		self._helper_thr.start()
+		self.__helper_thr.setDaemon(True)
+		self.__helper_thr.start()
 
 	@property
 	def prefix(self):
@@ -158,7 +155,9 @@ class RawReader(io.RawIOBase):
 				setattr(obj, key, old)
 
 	def _check_end_or_interrupt(self):
-		if self._isend:
+		if self.__isinterrupt:
+			raise KeyboardInterrupt()
+		if self.__isclose:
 			raise EOFError()
 
 	def flush(self):
@@ -172,6 +171,9 @@ class RawReader(io.RawIOBase):
 
 	def seekable(self):
 		return False
+
+	def fileno(self):
+		return self._reader.fileno()
 
 	def _flush_empty_buf(self):
 		self._check_end_or_interrupt()
@@ -234,10 +236,11 @@ class RawReader(io.RawIOBase):
 			self.__write(MOVE_LEFT_CHAR * (lline - self.__line_index))
 			self._writer.flush()
 
-	def __readchar(self):
-		if IS_WIN32:
+	if IS_WIN32:
+		def __readchar(self):
 			return msvcrt.getch()
-		else:
+	else:
+		def __readchar(self):
 			return self.__read(1)
 
 	def __read(self, size=-1):
@@ -302,7 +305,8 @@ class RawReader(io.RawIOBase):
 			line = ''.join(self.__line_buf)
 			self.__line_buf.clear()
 			self.__line_index = 0
-			self._histories.append(line)
+			if len(self._histories) == 0 or self._histories[-1] != line:
+				self._histories.append(line)
 			self.__current_his = 0
 			with self._empty_lock:
 				index = self._buffer.tell()
@@ -345,13 +349,15 @@ class RawReader(io.RawIOBase):
 			with self._write_lock:
 				self.__write(self._prefix)
 				self._writer.flush()
-			while not self._isend:
+			while not self.__isclose:
 				ch = self.__readchar()
-				if self._isend:
+				if self.__isclose:
 					break
 				chid = ord(ch)
 				if chid == BREAK_ID:
-					continue
+					self.__isinterrupt = True
+					break
+				if isinstance(ch, bytes): ch = try_decodes(ch, ['utf-8', 'gbk'])
 				with self._write_lock:
 					if chid == ESC_ID:
 						ch2 = self.__readchar()
@@ -362,7 +368,7 @@ class RawReader(io.RawIOBase):
 						self.__helper_delete_char()
 					elif chid == TAB_ID:
 						pass
-					elif ch == '\n':
+					elif ch == '\r' or ch == '\n':
 						self.__helper_on_enter()
 					elif ch.isprintable():
 						self.__helper_insert_char(ch)
@@ -370,13 +376,13 @@ class RawReader(io.RawIOBase):
 		except OSError:
 			pass
 		finally:
-			self.end()
+			self.close()
 
 	def __enter__(self):
 		return self
 
 	def __exit__(self, err_cls, err_msg, traceback):
-		self.close_()
+		self.close()
 		if err_cls is not None and not isinstance(err_cls, EOFError):
 			return False
 		return True
@@ -387,66 +393,101 @@ class RawReader(io.RawIOBase):
 
 	@property
 	def closed(self):
-		return self._isend
+		return self.__isclose
 
 	def close(self):
-		pass
-
-	def close_(self):
-		self.end()
-		if self._helper_thr is not None:
-			self._helper_thr = None
-
-	def end(self):
-		if self._isend:
+		if self.__pid != os.getpid():
 			return
-		self._isend = True
+		if self.__isclose:
+			return
+		self.__isclose = True
 		self.unload_mixin()
-		if not IS_WIN32:
-			if self._old_settings:
-				termios.tcsetattr(self._reader_fd, termios.TCSADRAIN, self._old_settings)
+		if self.__old_settings is not None:
+			termios.tcsetattr(self._reader_fd, termios.TCSADRAIN, self.__old_settings)
+			self.__old_settings = None
 		with self._empty_lock:
 			self._empty_lock.notify_all()
+		if self.__helper_thr is not None:
+			self.__helper_thr = None
 
+	def __del__(self):
+		if self.__pid == os.getpid():
+			self.close()
 
-# def _mixin_ConsoleHandler_tick():
-# 	def tick(self):
-# 		try:
-# 			text = ''
-# 			while True:
-# 				try:
-# 					text = read_write.poll()
-# 				except (EOFError, OSError, ValueError):
-# 					time.sleep(0.05)
-# 					continue
-# 				if text:
-# 					break
-# 			parsed_result: Info
-# 			try:
-# 				parsed_result = self.mcdr_server.server_handler_manager.get_current_handler().parse_console_command(text)
-# 			except:
-# 				self.mcdr_server.logger.exception(self.mcdr_server.tr('console_handler.parse_fail', text))
-# 			else:
-# 				if self.mcdr_server.logger.should_log_debug(DebugOption.HANDLER):
-# 					self.mcdr_server.logger.debug('Parsed text from {}:'.format(type(self).__name__), no_check=True)
-# 					for line in parsed_result.format_text().splitlines():
-# 						self.mcdr_server.logger.debug('    {}'.format(line), no_check=True)
-# 				self.mcdr_server.reactor_manager.put_info(parsed_result)
-# 		except (KeyboardInterrupt, EOFError, SystemExit, IOError) as error:
-# 			if self.mcdr_server.is_server_running():
-# 				self.mcdr_server.logger.critical('Critical exception caught in {}: {} {}'.format(type(self).__name__, type(error).__name__, error))
-# 				self.mcdr_server.interrupt()
-# 		except:
-# 			self.mcdr_server.logger.exception(self.mcdr_server.tr('console_handler.error'))
-# 	ConsoleHandler.tick = tick
+def try_decodes(string, encodes):
+	for e in encodes:
+		try:
+			return string.decode(e)
+		except UnicodeDecodeError:
+			pass
+	raise UnicodeDecodeError('Failed to decode {}, with encodes {}'.format(repr(string), str(encodes)))
+
+######### Mixins #########
+
+def _mixin_ConsoleHandler_tick():
+	from mcdreforged.executor.console_handler import ConsoleHandler
+	from mcdreforged.info import Info
+	from mcdreforged.utils.logger import DebugOption, SyncStreamHandler
+	def tick(self):
+		try:
+			text = input()
+			parsed_result: Info
+			try:
+				parsed_result = self.mcdr_server.server_handler_manager.get_current_handler().parse_console_command(text)
+			except:
+				self.mcdr_server.logger.exception(self.mcdr_server.tr('console_handler.parse_fail', text))
+			else:
+				if self.mcdr_server.logger.should_log_debug(DebugOption.HANDLER):
+					self.mcdr_server.logger.debug('Parsed text from {}:'.format(type(self).__name__), no_check=True)
+					for line in parsed_result.format_text().splitlines():
+						self.mcdr_server.logger.debug('    {}'.format(line), no_check=True)
+				self.mcdr_server.reactor_manager.put_info(parsed_result)
+		except KeyboardInterrupt:
+			if self.mcdr_server.is_server_running() and not self.mcdr_server.is_interrupt():
+				self.mcdr_server.interrupt()
+		except (EOFError, SystemExit, IOError) as error:
+			if self.mcdr_server.is_server_running():
+				self.mcdr_server.logger.critical('Critical exception caught in {}: {} {}'.format(type(self).__name__, type(error).__name__, error))
+				self.mcdr_server.interrupt()
+		except:
+			self.mcdr_server.logger.exception(self.mcdr_server.tr('console_handler.error'))
+	ConsoleHandler.tick = tick
+
+def _mixin_InfoReactorManager_put_info():
+	from mcdreforged.info_reactor.info_reactor_manager import InfoReactorManager
+	import queue
+	def put_info(self, info):
+		info.attach_mcdr_server(self.mcdr_server)
+		# echo info from the server to the console
+		if info.is_from_server:
+			if info.content != 'No player was found':
+				self.server_logger.info(info.raw_content)
+		try:
+			self.mcdr_server.task_executor.enqueue_info_task(lambda: self.process_info(info), info.is_user)
+		except queue.Full:
+			current_time = time.time()
+			logging_method = self.mcdr_server.logger.debug
+			kwargs = {'option': DebugOption.REACTOR}
+			if self.last_queue_full_warn_time is None or current_time - self.last_queue_full_warn_time >= constant.REACTOR_QUEUE_FULL_WARN_INTERVAL_SEC:
+				logging_method = self.mcdr_server.logger.warning
+				kwargs = {}
+				self.last_queue_full_warn_time = current_time
+			logging_method(self.mcdr_server.tr('info_reactor_manager.info_queue.full'), **kwargs)
+	InfoReactorManager.put_info = put_info
+
+def mixin_mcdr():
+	_mixin_ConsoleHandler_tick()
+	_mixin_InfoReactorManager_put_info()
 
 #################
 
 def on_load(server :MCDR.ServerInterface, prev_module):
 	global SERVER_OBJ, read_write
 	SERVER_OBJ = server
-	# _mixin_ConsoleHandler_tick()
-	if prev_module is not None:
+	mixin_mcdr()
+	if prev_module is None:
+		log_info('RawInput is on load')
+	else:
 		log_info('RawInput is on reload')
 		if prev_module.read_write is not None:
 			read_write = prev_module.read_write
@@ -455,11 +496,15 @@ def on_load(server :MCDR.ServerInterface, prev_module):
 			# 	prev_module.read_write.close()
 			# 	prev_module.read_write.reader.close()
 			# prev_module.read_write = None
-	else:
-		log_info('RawInput is on load')
+
+	if read_write is None:
 		sys_stdin = sys.stdin
-		sys.stdin = read_write = RawReader(os.fdopen(sys_stdin.fileno(), 'r'), sys.stdout, config['prefix'], mixin=True)
-		sys_stdin.close()
+		try:
+			sys.stdin = read_write = RawReader(os.fdopen(sys_stdin.fileno(), 'r'), sys.stdout, config['prefix'], mixin=True)
+		except AssertionError:
+			server.logger.error('The input stream are not atty')
+		else:
+			sys_stdin.close()
 
 #####!!!!!Has some bug
 # def on_unload(server :MCDR.ServerInterface):
@@ -491,7 +536,7 @@ def on_mcdr_stop(server :MCDR.ServerInterface):
 	log_info('mcdr is on stop')
 	if read_write is not None:
 		if not read_write.closed:
-			read_write.close_()
+			read_write.close()
 			threading.Thread(target=lambda: os.close(read_write.reader.fileno()), daemon=True).start() # I don't know why it can be blocked
 		read_write = None
 	log_info('closed')
